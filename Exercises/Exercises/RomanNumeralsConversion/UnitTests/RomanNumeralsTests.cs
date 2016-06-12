d5abda54299cdc76a8a73cd6dc5707ece6fdda64
Mahmut Surekci <msurekci@dctfh.com>
2016-06-12 10:12:21 +0100

    Roman numerals to numbers

---
+++ Exercises/Exercises/RomanNumeralsConversion/UnitTests/RomanNumeralsTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using RomanNumeralsConversion;
+
+namespace UnitTests
+{
+    public class RomanNumeralsTests
+    {
+        [TestCase(null, null)]
+        [TestCase("aaa", null)]
+        [TestCase("I", 1)]
+        [TestCase("II", 2)]
+        [TestCase("III", 3)]
+        [TestCase("IIII", 4)]//shouldnt
+        [TestCase("IV", 4)]
+        [TestCase("V", 5)]
+        
+        [TestCase("VI", 6)]
+        [TestCase("VIII", 8)]
+        [TestCase("IX", 9)]
+        [TestCase("X", 10)]
+        [TestCase("XI", 11)]
+        public void RomanNumeralShouldBeConvertedToCorrectArabicNumbers(string romanNumeral, int expectedArabicNumber)
+        {
+            var result = new RomanNumeralConverter().Convert(romanNumeral);
+
+            Assert.AreEqual(expectedArabicNumber, result);
+        }
+    }
+}
---
+++ Exercises/UnitTest/RomanNumerals/RomanNumeralsToNumbersTests.cs
@@ -0,0 +1,28 @@
+﻿using Exercises.RomanNumerals;
+using NUnit.Framework;
+
+namespace UnitTest.RomanNumerals
+{
+    public class RomanNumeralsToNumbersTests
+    {
+        [TestCase(null, null)]
+        [TestCase("aaa", null)]
+        [TestCase("I", 1)]
+        [TestCase("II", 2)]
+        [TestCase("III", 3)]
+        [TestCase("IIII", 4)]//shouldnt
+        [TestCase("IV", 4)]
+        [TestCase("V", 5)]
+        [TestCase("VI", 6)]
+        [TestCase("VIII", 8)]
+        [TestCase("IX", 9)]
+        [TestCase("X", 10)]
+        [TestCase("XI", 11)]
+        public void RomanNumeralShouldBeConvertedToCorrectArabicNumbers(string romanNumeral, int expectedArabicNumber)
+        {
+            var result = new RomanNumeralConverter().Convert(romanNumeral);
+
+            Assert.AreEqual(expectedArabicNumber, result);
+        }
+    }
+}
